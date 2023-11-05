ef817226c601229f147a1db1c6e3ffabb5b270b3
shaoming1995 <116997703+shaoming1995@users.noreply.github.com>
2023-11-06 03:58:57 +0800

    Add files via upload

--- R/local_clump_data.R
+++ R/local_clump_data.R
@@ -1,51 +1,105 @@
 #' @title 提供本地聚类分析的R包，但是需要联系顶刊研习社获取参考面板数据或者微信SFM19950928
 #' @param keyssh 输入密钥
 #' @param temp_dat 需要聚类分析的变量名
-#' @param filepos 参考面板数据（.bed/.fam/.bim）的文件位置
+#' @param filepath 参考面板数据（.bed/.fam/.bim）的文件位置
 #' @param pop 人种选择，仅支持EUR/EAS
 #' @param clump_kb 聚类的kb
 #' @param clump_r2 聚类的r2
 #' @export
 
-local_clump_data<-function(keyssh,temp_dat,filepos,pop,clump_kb,clump_r2){
+local_clump_data<-function(keyssh,temp_dat,filepath,pop,clump_kb,clump_r2){
   if(!require("tidyr",quietly=T))
       install.packages("tidyr")
   if(!require("plinkbinr",quietly=T))
     devtools::install_github("explodecomputer/plinkbinr")
-  if(!require("TwoSampleMR",quietly=T))
-    devtools::install_github("MRCIEU/TwoSampleMR")
-  if(!require("ieugwasr",quietly=T))
-  devtools::install_github("mrcieu/ieugwasr")
-  if(!require("sofmkey",quietly=T))
-    devtools::install_github("shaoming1995/sofmkey")
   library(plinkbinr)
-  library(TwoSampleMR)
-  library(ieugwasr)
   library(tidyr)
   library(key)
-  K<-key(keyssh)
-  if(K==9544){
- remove.packages("key")
-  # 补充ld_clump需要的三列
-  temp_dat$id <- temp_dat$id.exposure # id列用于区分是单个或多个来源的gwas数据，clump按照单个gwas数据进行
+  if(Sys.info()==keyssh){
+  temp_dat$id <- temp_dat$id.exposure
   temp_dat$rsid <- temp_dat$SNP
   temp_dat$pval <- temp_dat$pval.exposure
-  # 执行ld_clump
-  # bfile指定参考文件的路径
-
-  temp_dat <- ld_clump(temp_dat,
-                       # get_plink_exe()
+  filepath1<-paste0(filepath,"/",pop)
+  ld_sofm<-function (dat = NULL, clump_kb = 10000, clump_r2 = 0.001, clump_p = 0.99,
+            pop = "EUR", access_token = NULL, bfile = NULL, plink_bin = NULL)
+  {
+    stopifnot("rsid" %in% names(dat))
+    stopifnot(is.data.frame(dat))
+    if (is.null(bfile)) {
+      message("Please look at vignettes for options on running this locally if you need to run many instances of this command.")
+    }
+    if (!"pval" %in% names(dat)) {
+      if ("p" %in% names(dat)) {
+        warning("No 'pval' column found in dat object. Using 'p' column.")
+        dat[["pval"]] <- dat[["p"]]
+      }
+      else {
+        warning("No 'pval' column found in dat object. Setting p-values for all SNPs to clump_p parameter.")
+        dat[["pval"]] <- clump_p
+      }
+    }
+    if (!"id" %in% names(dat)) {
+      dat$id <- random_string(1)
+    }
+    if (is.null(bfile)) {
+      access_token = check_access_token()
+    }
+    ids <- unique(dat[["id"]])
+    res <- list()
+    for (i in 1:length(ids)) {
+      x <- subset(dat, dat[["id"]] == ids[i])
+      if (nrow(x) == 1) {
+        message("Only one SNP for ", ids[i])
+        res[[i]] <- x
+      }
+      else {
+        message("Clumping ", ids[i], ", ", nrow(x), " variants, using ",
+                pop, " population reference")
+        if (is.null(bfile)) {
+          res[[i]] <- ld_clump_api(x, clump_kb = clump_kb,
+                                   clump_r2 = clump_r2, clump_p = clump_p, pop = pop,
+                                   access_token = access_token)
+        }
+        else {
+          res[[i]] <- ld_clump_local(x, clump_kb = clump_kb,
+                                     clump_r2 = clump_r2, clump_p = clump_p, bfile = bfile,
+                                     plink_bin = plink_bin)
+        }
+      }
+    }
+    res <- dplyr::bind_rows(res)
+    return(res)
+  }
+  temp_dat <- ld_sofm(temp_dat,
                        plink_bin = get_plink_exe(),
-                       bfile = filepos,
+                       bfile = filepath1,
                        clump_kb = clump_kb, clump_r2 = clump_r2,pop=pop)
-  # 运行结果：
-  # Removing 69 of 92 variants due to LD with other variants or absence from LD reference panel
-  # 移除重复的列：rsid，pval
   temp_dat$rsid <- NULL
   temp_dat$pval <- NULL
   return(temp_dat)
   cat("已经完成聚类分析")}
   else{cat("keyssh不正确,请联系管理员微信SFM19950928获取密钥")}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
